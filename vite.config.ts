49fcf5d2536248c2b090eb5e0ad245c9400a193a
Danila Petrov <d.a.petrov08@gmail.com>
2024-08-25 15:58:04 +0300

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,59 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    target: 'es6',
-    rollupOptions: {
-      input: {
-        'open-dnd': 'index.html',
-        initConfig: 'src/app/initConfig.ts'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const taskName = env.TASK_NAME;
+  const openeduUrl = env.OPENEDU_URL;
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: "transformToOpenEdu",
+        transformIndexHtml: {
+          order: "post",
+          handler: async (htmlString) => {
+            if (!taskName || !openeduUrl) {
+              throw new Error('Bad env config')
+            }
+
+            let result = htmlString;
+            result = result.replace('/open-dnd.js', openeduUrl + 'open-dnd.js')
+            result = result.replace('/initConfig.js', openeduUrl + taskName + '.js')
+            result = result.replace('/open-dnd.css', openeduUrl + 'open-dnd.css')
+            return result
+          },
+        },
       },
-      output: {
-        entryFileNames: "[name].js",
-        assetFileNames: "[name][extname]"
+      {
+        name: 'rename index.html',
+        enforce: 'post',
+        generateBundle: (_options, bundle) => {
+          bundle['initConfig.js'].fileName = taskName + '.js'
+          bundle['index.html'].fileName = taskName + '.html'
+        }
+      }
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    build: {
+      target: "es6",
+      rollupOptions: {
+        input: {
+          "open-dnd": "index.html",
+          initConfig: "src/app/initConfig.ts",
+        },
+        output: {
+          entryFileNames: "[name].js",
+          assetFileNames: "[name][extname]",
+        },
+      },
+    },
+  };
 });
